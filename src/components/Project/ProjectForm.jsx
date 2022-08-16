ae26374e6141762814ad64d49db5dc70eb1a789b
Luk4x <luk4xm4ci3l@gmail.com>
2022-08-16 19:29:19 -0300

    Home, Navbar, CreateProject and Container style updated, form started, form input componentization started

--- src/components/Project/ProjectForm.jsx
+++ src/components/Project/ProjectForm.jsx
@@ -1,20 +1,15 @@
 import styles from './ProjectForm.module.css';
+import Input from '../form/Input';
 
 export default function ProjectForm() {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do Projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-                <select name="category_id" id="category_id">
-                    Selecione a categoria
-                </select>
-            </div>
-            <button>Criar Projeto</button>
+        <form className={styles.form}>
+            <Input type="text" name="formProjectName" placeholder="Insira o nome do Projeto" labelContent="Nome do Projeto" />
+            <Input type="number" name="formProjectBudget" placeholder="Insira o orçamento total" labelContent="Orçamento do Projeto" />
+            <select name="category_id" id="category_id">
+                Selecione a categoria
+            </select>
+            <input type="button" value="Criar Projeto" />
         </form>
     );
 }
