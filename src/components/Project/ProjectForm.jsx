723a9314c5edb3d168adb2871706bef9fb520f6b
Luk4x <luk4xm4ci3l@gmail.com>
2022-08-28 14:01:49 -0300

    json-server backend created, form completed

--- src/components/Project/ProjectForm.jsx
+++ src/components/Project/ProjectForm.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from 'react';
+
 import styles from './ProjectForm.module.css';
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import LinkButton from '../layout/LinkButton';
 
 export default function ProjectForm() {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <form className={styles.form}>
             <Input type="text" name="formProjectName" placeholder="Insira o nome do Projeto" labelContent="Nome do Projeto" />
             <Input type="number" name="formProjectBudget" placeholder="Insira o orçamento total" labelContent="Orçamento do Projeto" />
-            <Select name="category" labelContent="Selecione uma categoria" />
-            <LinkButton type="submit">Criar Projeto</LinkButton>
+            <Select name="category" labelContent="Selecione uma categoria" options={categories} />
+            <LinkButton to="#" type="submit">
+                Criar Projeto
+            </LinkButton>
         </form>
     );
 }
--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -1,11 +1,15 @@
 import styles from './Select.module.css';
 
-export default function Select({ name, labelContent }) {
+export default function Select({ name, labelContent, options }) {
     return (
         <div className={styles.selectContainer}>
             <label htmlFor={name}>{labelContent}:</label>
             <select name={name} id={name}>
-                <option>{labelContent}</option>
+                {options.map(option => (
+                    <option key={option.id} value={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     );
