0be82d346cfccb77f44c3457884fe90cd348ff14
Luk4x <luk4xm4ci3l@gmail.com>
2022-09-15 18:13:04 -0300

    navbar link hightlight removed

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,54 +1,8 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import { FaMonero } from 'react-icons/fa';
-import { useState } from 'react';
 
 export default function Navbar() {
-    const [activeHome, setActiveHome] = useState(true);
-    const [activeProjects, setActiveProjects] = useState();
-    const [activeCompany, setActiveCompany] = useState();
-    const [activeContact, setActiveContact] = useState();
-
-    const handleToggleLink = linkNumber => {
-        switch (linkNumber) {
-            case 1:
-                setActiveHome(true);
-                setActiveProjects(false);
-                setActiveCompany(false);
-                setActiveContact(false);
-                break;
-
-            case 2:
-                setActiveHome(false);
-                setActiveProjects(true);
-                setActiveCompany(false);
-                setActiveContact(false);
-                break;
-
-            case 3:
-                setActiveHome(false);
-                setActiveProjects(false);
-                setActiveCompany(true);
-                setActiveContact(false);
-                break;
-
-            case 4:
-                setActiveHome(false);
-                setActiveProjects(false);
-                setActiveCompany(false);
-                setActiveContact(true);
-                break;
-
-            default:
-                setActiveHome(false);
-                setActiveProjects(false);
-                setActiveCompany(false);
-                setActiveContact(false);
-                console.log(linkNumber);
-                break;
-        }
-    };
-
     return (
         <nav className={styles.navbar}>
             <Link to="/">
@@ -56,24 +10,16 @@ export default function Navbar() {
             </Link>
             <ul className={styles.list}>
                 <li>
-                    <Link onClick={() => handleToggleLink(1)} className={activeHome ? styles.activeLink : null} to="/">
-                        Home
-                    </Link>
+                    <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link onClick={() => handleToggleLink(2)} className={activeProjects ? styles.activeLink : null} to="/projects">
-                        Projetos
-                    </Link>
+                    <Link to="/projects">Projetos</Link>
                 </li>
                 <li>
-                    <Link onClick={() => handleToggleLink(3)} className={activeCompany ? styles.activeLink : null} to="/company">
-                        Sobre
-                    </Link>
+                    <Link to="/company">Sobre</Link>
                 </li>
                 <li>
-                    <Link onClick={() => handleToggleLink(4)} className={activeContact ? styles.activeLink : null} to="/contact">
-                        Contato
-                    </Link>
+                    <Link to="/contact">Contato</Link>
                 </li>
             </ul>
         </nav>
