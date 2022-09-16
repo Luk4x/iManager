6db79c04e933fd56de2c3779f93c38e2efccfc02
Luk4x <luk4xm4ci3l@gmail.com>
2022-09-16 19:37:06 -0300

    simplicity company article o completed

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -1,6 +1,7 @@
 import styles from './Company.module.css';
 
 import SubmitButton from '../form/SubmitButton';
+import CompanyImage2 from '../../assets/companyImage2.jpg';
 
 import { BiDownArrow } from 'react-icons/bi';
 import ScrollAnimation from 'react-animate-on-scroll';
@@ -8,7 +9,7 @@ import ScrollAnimation from 'react-animate-on-scroll';
 export default function Company() {
     return (
         <section className={styles.section}>
-            <article className={`${styles.width100Vw} ${styles.headerImage}`}>
+            <article className={styles.headerImage}>
                 <h2>Agente respira o novo</h2>
                 <a href="#colorHighlight">
                     <SubmitButton>
@@ -16,7 +17,7 @@ export default function Company() {
                     </SubmitButton>
                 </a>
             </article>
-            <article id="colorHighlight" className={`${styles.width100Vw} ${styles.colorHighlight}`}>
+            <article id="colorHighlight" className={styles.colorHighlight}>
                 <ScrollAnimation animateIn="fadeIn" animateOnce="true">
                     <h3>Nascemos para fazer diferente</h3>
                     <p>
@@ -24,6 +25,15 @@ export default function Company() {
                         <br />
                         Para reunir em um Web App tudo que faz diferença na gestão da sua empresa.
                     </p>
+                </ScrollAnimation>
+            </article>
+            <article className={styles.simplicity}>
+                <ScrollAnimation animateIn="fadeIn" animateOnce="true">
+                    <div>
+                        <h3>Criamos o que simplifica a gestão da sua empresa</h3>
+                        <p>Simplificar é tirar as burocracias da frente e conectar as soluções que a sua empresa precisa em um só lugar.</p>
+                    </div>
+                    <img src={CompanyImage2} alt="woman programmer working" />
                 </ScrollAnimation>
             </article>
         </section>
