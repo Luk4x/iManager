482866a45ba818bfe354451a5d7eef7de1896312
Luk4x <luk4xm4ci3l@gmail.com>
2022-09-19 14:07:44 -0300

    comapny sustainable and recognition section completed

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -3,6 +3,12 @@ import styles from './Company.module.css';
 import SubmitButton from '../form/SubmitButton';
 import CompanyImage2 from '../../assets/companyImage2.jpg';
 import CompanyImage4 from '../../assets/companyImage4.jpg';
+import CompanyImage6 from '../../assets/companyImage6.jpg';
+import iBestImage from '../../assets/iBestImage.png';
+import reclameAquiImage from '../../assets/reclameAquiImage.png';
+import joaoVitorMeninImage from '../../assets/joaoVitorMeninImage.png';
+import humanizadasImage from '../../assets/humanizadasImage.png';
+import seloOuroGHGImage from '../../assets/seloOuroGHGImage.png';
 
 import { BiDownArrow } from 'react-icons/bi';
 import { MdMonitor, MdAccountCircle, MdLock, MdAirplaneTicket, MdNetworkCell, MdOutlineSecurity } from 'react-icons/md';
@@ -11,6 +17,7 @@ import { CgMathPlus } from 'react-icons/cg';
 import { RiShoppingBagFill } from 'react-icons/ri';
 import { BsCreditCard2FrontFill, BsCheckLg } from 'react-icons/bs';
 
+import 'animate.css/animate.min.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -47,7 +54,7 @@ export default function Company() {
                 </a>
             </article>
             <article id="colorHighlight" className={styles.colorHighlight}>
-                <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+                <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true} duration={1.6} delay={100} animatePreScroll={false}>
                     <h3>Nascemos para fazer diferente</h3>
                     <p>
                         A iManager está aqui para fazer aquilo que ninguém conseguiu...
@@ -57,13 +64,13 @@ export default function Company() {
                 </ScrollAnimation>
             </article>
             <article className={styles.simplicity}>
-                <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
-                    <div>
-                        <h3>Criamos o que simplifica a gestão das empresas</h3>
-                        <p>Simplificar é tirar as burocracias da frente e conectar as soluções que a sua empresa precisa em um só lugar.</p>
-                        <br />
-                        <p>Nós da iManager percebemos uma dificuldade das empresas viemos trazer a solução que ninguém trouxe: Reunimos em um Web App tudo o que a sua empresa precisa para ter uma gestão simples, segura e eficiente.</p>
-                    </div>
+                <ScrollAnimation animateIn="animate__fadeInUp" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
+                    <h3>Criamos o que simplifica a gestão das empresas</h3>
+                    <p>Simplificar é tirar as burocracias da frente e conectar as soluções que a sua empresa precisa em um só lugar.</p>
+                    <br />
+                    <p>Nós da iManager percebemos uma dificuldade das empresas viemos trazer a solução que ninguém trouxe: Reunimos em um Web App tudo o que a sua empresa precisa para ter uma gestão simples, segura e eficiente.</p>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="animate__fadeInRight" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
                     <img src={CompanyImage2} alt="woman programmer working" />
                 </ScrollAnimation>
             </article>
@@ -72,8 +79,10 @@ export default function Company() {
                 <p>Mais que um gerenciador, um Web App que simplifica a sua empresa.</p>
             </div>
             <article className={styles.services}>
-                <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+                <ScrollAnimation animateIn="animate__fadeInLeft" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
                     <img src={CompanyImage4} alt="woman accountant working" />
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="animate__fadeInDown" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
                     <div>
                         <h3>
                             Isso é <span>iManager</span>
@@ -114,7 +123,7 @@ export default function Company() {
                 <h3>
                     O <span>iManager</span> tem tudo que você precisa para gerir sua empresa.
                 </h3>
-                <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+                <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
                     <div className={styles.cardList}>
                         <div>
                             <MdOutlineSecurity />
@@ -166,7 +175,9 @@ export default function Company() {
                                 style={{
                                     transitionProperty: 'all',
                                     transition: '1s ease-out',
-                                    fontSize: 50
+                                    fontSize: 60,
+                                    borderBottom: '2px solid #b3679b',
+                                    color: '#4f5d75'
                                 }}
                                 duration={1200}
                                 formatValue={v => v.toFixed(0)}
@@ -183,7 +194,9 @@ export default function Company() {
                                 style={{
                                     transitionProperty: 'all',
                                     transition: '1s ease-out',
-                                    fontSize: 50
+                                    fontSize: 60,
+                                    borderBottom: '2px solid #b3679b',
+                                    color: '#4f5d75'
                                 }}
                                 duration={900}
                                 formatValue={v => v.toFixed(0)}
@@ -199,7 +212,9 @@ export default function Company() {
                                 style={{
                                     transitionProperty: 'all',
                                     transition: '1s ease-out',
-                                    fontSize: 50
+                                    fontSize: 60,
+                                    borderBottom: '2px solid #b3679b',
+                                    color: '#4f5d75'
                                 }}
                                 duration={1800}
                                 formatValue={v => v.toFixed(0)}
@@ -215,7 +230,7 @@ export default function Company() {
                     <h3>Nossa Trajetória</h3>
                     <p>Numa jornada com grandes transformações, cada momento é importante para continuarmos crescendo e inovando.</p>
                 </div>
-                <Carousel className={styles.carouselRoot} showArrows={true} showStatus={false} transitionTime={800} showThumbs={false}>
+                <Carousel className={styles.carouselRoot} showArrows={true} showStatus={false} transitionTime={900} showThumbs={false}>
                     <div>
                         <h4>
                             <span>&emsp;&emsp;</span>
@@ -281,6 +296,60 @@ export default function Company() {
                         <p>Migramos nossa base de dados e atingimos a marca de 20 milhões de cliente e lançamos a Global Account.</p>
                     </div>
                 </Carousel>
+            </article>
+            <article id="now" className={styles.sustainable}>
+                <ScrollAnimation animateIn="animate__fadeDown" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
+                    <h3>Ser sustentável é agilizar e incluir.</h3>
+                    <p>Geramos valor não só para nossos clientes, mas também para colaboradores, investidores e para a sociedade como um todo. Sempre alinhados com nossos pilares de gestão, Ambiental, Social e Governança.</p>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="animate__fadeRight" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
+                    <img src={CompanyImage6} alt="wind energy" />
+                </ScrollAnimation>
+            </article>
+            <article className={styles.recognition}>
+                <ScrollAnimation animateIn="animate__pulse" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
+                    <Carousel className={styles.secondCarousel} showArrows={true} showStatus={false} transitionTime={500} showThumbs={false}>
+                        <div className={styles.carouselItem}>
+                            <img src={iBestImage} alt="iBest award image" />
+                            <div>
+                                <h4>Prêmio iBest</h4>
+                                <p>Recebemos os Prêmios de Melhor Gestora Digital do Brasil pelo júri popular, Melhor Web App e Top 3 Gestores Digitais pelo júri da Academia iBest.</p>
+                            </div>
+                        </div>
+                        <div className={styles.carouselItem}>
+                            <img src={reclameAquiImage} alt="reclame aqui award image" />
+                            <div>
+                                <h4>Prêmio Reclame Aqui</h4>
+                                <p>Fomos eleitos o 2º melhor atendimento do Brasil por voto popular pelo segundo ano consecutivo - categoria de Gestores Digitais.</p>
+                            </div>
+                        </div>
+                        <div className={styles.carouselItem}>
+                            <img src={joaoVitorMeninImage} alt="João Vitor Menin image" />
+                            <div>
+                                <h4>Forbes Brasil</h4>
+                                <p>João Vitor Menin foi eleito um dos 10 líderes que melhor mantiveram ou elevaram a relevância de suas marcas e os indicadores de seus negócios em meio a crise.</p>
+                            </div>
+                        </div>
+                        <div className={styles.carouselItem}>
+                            <img src={humanizadasImage} alt="Humanizadas award image" />
+                            <div>
+                                <h4>Melhores para o Brasil</h4>
+                                <p>Fomos reconhecidos pela Humanizadas como uma organização aberta aos feedbacks e agente de transformação social e ambiental que soluciona problemas reais.</p>
+                            </div>
+                        </div>
+                        <div className={styles.carouselItem}>
+                            <img src={seloOuroGHGImage} alt="Selo Ouro GHG Protocol award image" />
+                            <div>
+                                <h4>Selo Ouro GHG Protocol</h4>
+                                <p>Pela segunda vez consecutiva o iManager teve seu Inventário de emissões de gases de Efeito Estufa (GEE) reconhecido com o Selo Ouro do GHG Protocol, reforçando nosso compromisso com o desenvolvimento sustentável.</p>
+                            </div>
+                        </div>
+                    </Carousel>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true} duration={1.8} delay={100} animatePreScroll={false}>
+                    <h3>Prêmios e reconhecimentos</h3>
+                    <p>Somos reconhecidos e premiados em diversas áreas, por nossas ações inovadoras e focadas em impactar positivamente a vida das pessoas, a gestão das empresas, e o mundo.</p>
+                </ScrollAnimation>
             </article>
         </section>
     );
