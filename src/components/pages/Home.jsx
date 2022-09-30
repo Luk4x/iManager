2338e65bd63bf0f81513a92bc1ed408cafa8bb03
Luk4x <luk4xm4ci3l@gmail.com>
2022-09-30 15:09:07 -0300

    json-server removed, pinging api on home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,22 @@ import LinkButton from '../layout/LinkButton';
 import { GiReceiveMoney } from 'react-icons/gi';
 import PurpleBackground from '../layout/PurpleBackground';
 
+import { useEffect } from 'react';
+
 export default function Home() {
+    // pinging API just to 'wake up' there in heroku
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_BASE_URL}/projects`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <section className={styles.sectionContainer}>
             <PurpleBackground />
